b7fae1c63182e453a5cc27abceeb1cf34e865fe0
shankar08 <shan08kar@gmail.com>
2017-01-05 00:18:02 -0500

    first time

---
+++ TorrentNewsApp/TorrentNewsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TorrentNewsApp
+//
+//  Created by Shankar Prajapati on 1/4/17.
+//  Copyright © 2017 Shankar Prajapati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
